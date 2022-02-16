b654aeb19327157b74f1d1087bb9014b98ceb2a3
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2022-02-16 17:15:04 -0300

    .

--- src/components/Pages/SolicitationItems.jsx
+++ src/components/Pages/SolicitationItems.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import Header from "../Header/Header";
 import Button from "../Buttons/Button";
@@ -10,28 +11,32 @@ import itemList from "../../data/itemsList.json";
 import segmentList from "../../data/segmentList.json";
 
 export default function SolicitationItems() {
+  const [formData, setFormData] = useState({
+    amount: 0,
+    unit: "",
+    item: "",
+    segment: "",
+  });
+
+  function handleClick(event) {
+    console.log(event.target);
+    console.log(OptionsSegmentList);
+    // const { name, value, type } = event.target;
+  }
+
   const OptionsUnitList = unitList.map(unit => {
     let k = Math.random();
-    return (
-    <option key={k}>
-      {unit.unit}
-    </option>);
+    return <option key={k}>{unit.unit}</option>;
   });
 
   const OptionsItemList = itemList.map(item => {
     let k = Math.random();
-    return (
-    <option key={k}>
-      {item.item}
-    </option>);
+    return <option key={k}>{item.item}</option>;
   });
 
   const OptionsSegmentList = segmentList.map(segment => {
     let k = Math.random();
-    return (
-    <option key={k}>
-      {segment.segment}
-    </option>);
+    return <option key={k}>{segment.segment}</option>;
   });
 
   return (
@@ -41,20 +46,25 @@ export default function SolicitationItems() {
       <div className="formContainer">
         <form action="">
           <label htmlFor="amount">Quantidade:</label>
-          <input type="number" name="amount" id="amount" />
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            onChange={handleClick}
+          />
 
           <label htmlFor="unit">Unidade:</label>
-          <select name="unit" id="unit">
+          <select name="unit" id="unit" onChange={handleClick}>
             {OptionsUnitList}
           </select>
 
           <label htmlFor="item">Item:</label>
-          <select name="item" id="item">
+          <select name="item" id="item" onChange={handleClick}>
             {OptionsItemList}
           </select>
 
           <label htmlFor="segment">Segmento:</label>
-          <select name="segment" id="segment">
+          <select name="segment" id="segment" onChange={handleClick}>
             {OptionsSegmentList}
           </select>
         </form>
